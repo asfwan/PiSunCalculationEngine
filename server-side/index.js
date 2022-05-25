cbdb4c4506078d6d51068a049b6d84de64694ac9
ahmad saufwan sazali <asfwan@gmail.com>
2022-05-25 23:27:00 +0800

    cant use es syntax on express

--- server-side/index.js
+++ server-side/index.js
@@ -1,19 +1,21 @@
-import express, { json, urlencoded } from "express";
-import { createPool } from "mysql2";
-import cors from "cors";
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
-const db = createPool({
+const db = mysql.createPool({
   host: "node_mysql", // the host name MYSQL_DATABASE: node_mysql
   user: "MYSQL_USER", // database user MYSQL_USER: MYSQL_USER
   password: "MYSQL_PASSWORD", // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
   database: "pi_sun", // database name MYSQL_HOST_IP: pi_sun
 });
 
-app.use(cors());
 const app = express();
-app.use(json());
-app.use(urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Pi Test");
 });
+
+app.listen("3001", () => {});
