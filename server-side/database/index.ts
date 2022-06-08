76bea5e4550c0a8f414929eda319411560dfb87d
ahmad saufwan sazali <asfwan@gmail.com>
2022-06-08 22:38:11 +0800

    db check added

--- server-side/database/index.ts
+++ server-side/database/index.ts
@@ -2,9 +2,14 @@ import mysql from "mysql2";
 
 export const database = mysql.createPool({
   host: "localhost", // the host name MYSQL_DATABASE: node_mysql
-  user: "MYSQL_USER", // database user MYSQL_USER: MYSQL_USER
-  password: "MYSQL_PASSWORD", // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
+  user: "mysql_user", // database user MYSQL_USER: MYSQL_USER
+  password: "mysql_pass", // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
   database: "pi_sun", // database name MYSQL_HOST_IP: pi_sun
+});
+
+database.query("select count(*) as count from pi_values", (err, res) => {
+  if (err) return console.log("db is not ok");
+  console.log("db is ok");
 });
 
 // module.exports = database;
